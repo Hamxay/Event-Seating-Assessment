d74234fb304c5ab07da5e10176b1f3f810d4b0a3
Hamxay <hamzach1288@gmail.com>
2025-11-19 21:33:33 +0500

    initialization

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Venue from "@/src/module/components";
+
+const VenuePage = () => {
+  return (
+    <main className="min-h-screen bg-background">
+      <Venue />
+    </main>
+  );
+};
+
+export default VenuePage;
